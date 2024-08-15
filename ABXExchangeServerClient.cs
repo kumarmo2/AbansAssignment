24666148d79f2d810be12077fb15616287032042
Mohit <kr.mohit6794@gmail.com>
2024-08-15 10:43:16 +0530

    writing ExchangeClient

---
+++ ABXExchangeServerClient.cs
@@ -0,0 +1,62 @@
+namespace ABXConsoleClient;
+using System.Net.Sockets;
+
+public class ABXExchangeServerClient : IDisposable
+{
+    private TcpClient _tcp;
+    private readonly string _address;
+    // TODO: see if its possible to replace with buffStream
+    private NetworkStream _stream;
+    private readonly int _port;
+
+    public ABXExchangeServerClient(string address, int port)
+    {
+        _tcp = new TcpClient();
+        _address = address;
+        _port = port;
+    }
+
+
+    private void MakeSureTCPConnectionIsConnected()
+    {
+        if (!_tcp.Connected)
+        {
+            // Console.WriteLine(">>>>>> tcp is not connected");
+            _tcp = new TcpClient();
+            _tcp.Connect(_address, _port);
+            Console.WriteLine($"now is it connected, {_tcp.Connected}");
+            _stream = _tcp.GetStream();
+            _stream.ReadTimeout = 500;
+        }
+        else
+        {
+            Console.WriteLine(">>>>>> tcp is already connected");
+        }
+    }
+
+    public int Read(Span<byte> buf)
+    {
+        MakeSureTCPConnectionIsConnected();
+        return _stream.Read(buf);
+    }
+
+    public void WriteRequest(ABXRequest request)
+    {
+        MakeSureTCPConnectionIsConnected();
+        request.WriteToSTream(_stream);
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine("...dispose called");
+        // TODO: Implement dispose correctly.
+        if (_stream is not null)
+        {
+            _stream.Dispose();
+        }
+        if (_tcp is not null)
+        {
+            _tcp.Dispose();
+        }
+    }
+}
--- Worker.cs
+++ Worker.cs
@@ -21,126 +21,156 @@ public class Worker
         var address = "127.0.0.1";
         var port = 3000;
         List<ABXResponsePacket> intermediateResults = new List<ABXResponsePacket>();
-        while (!doneAllStreamRequest)
+        using (var client = new ABXExchangeServerClient(address, port))
         {
-            var result = ExecuteAllStreamRequest(address, port);
-            _logger.LogInformation(".... here");
-            if (result.Err is not null)
+            while (!doneAllStreamRequest)
             {
-                _logger.LogInformation("...retrting");
-                continue;
-            }
-            intermediateResults = result.Ok;
-            doneAllStreamRequest = true;
-        }
-        intermediateResults.Sort((first, second) =>
-        {
-            return (int)first.Sequence - (int)second.Sequence;
-        });
-
-        var lastSeq = intermediateResults[intermediateResults.Count - 1].Sequence;
-        _logger.LogInformation("lastSeq: {lastSeq}", lastSeq);
-
-        var presentSequences = intermediateResults.ToDictionary(i => i.Sequence);
-        var finalResult = new List<ABXResponsePacket>();
-
-        Span<byte> singlePacketBuffer = stackalloc byte[ABXResponsePacket.PACKET_SIZE_BYTES];
-        var client = new TcpClient();
-        client.Connect(address, port);
-        uint i = 1;
-        while (i <= lastSeq)
-        {
-            if (presentSequences.ContainsKey(i))
-            {
-                finalResult.Add(presentSequences[i]);
-                i++;
-                continue;
-            }
-            try
-            {
-                _logger.LogInformation("packet i: {i} is missing, isClientConnected: {isClientConnected}", i, client.Connected);
-                if (!client.Connected)
+                var result = ExecuteAllStreamRequest(client);
+                if (result.Err is not null)
                 {
-                    client = new TcpClient();
-                    client.Connect(address, port);
-                    // _logger.LogInformation("connected again, isClientConnected: {isClientConnected}", client.Connected);
+                    // _logger.LogInformation("...retrying");
+                    continue;
                 }
-                using (var stream = client.GetStream())
+                intermediateResults = result.Ok;
+                doneAllStreamRequest = true;
+            }
+            _logger.LogInformation(">>>> stream was fetched <<<<");
+            Thread.Sleep(5000);
+            intermediateResults.Sort((first, second) =>
+                    {
+                        return (int)first.Sequence - (int)second.Sequence;
+                    });
+
+            var lastSeq = intermediateResults[intermediateResults.Count - 1].Sequence;
+            _logger.LogInformation("lastSeq: {lastSeq}", lastSeq);
+
+            var presentSequences = intermediateResults.ToDictionary(i => i.Sequence);
+            var finalResult = new List<ABXResponsePacket>();
+
+            Span<byte> singlePacketBuffer = stackalloc byte[ABXResponsePacket.PACKET_SIZE_BYTES];
+            uint i = 1;
+            while (i <= lastSeq)
+            {
+                if (presentSequences.ContainsKey(i))
                 {
+                    finalResult.Add(presentSequences[i]);
+                    i++;
+                    continue;
+                }
+                try
+                {
+                    // _logger.LogInformation("packet i: {i} is missing, isClientConnected: {isClientConnected}", i, client.Connected);
+                    _logger.LogInformation("packet i: {i} is missing", i);
+                    Thread.Sleep(5000);
                     // _logger.LogInformation(">>> got the stream");
-                    stream.ReadTimeout = 500;
                     ABXRequest request = new ABXRequest { CallType = ABXRequest.RESEND_PACKET, ResendSeq = (byte)i };
                     // _logger.LogInformation("writing request for missing ResendSeq, seq: {i}", i);
-                    request.WriteToSTream(stream);
-                    // _logger.LogInformation(">>>>> write done");
+                    client.WriteRequest(request);
+                    // request.WriteToSTream(stream);
+                    _logger.LogInformation(">>>>> write done, will read now after sleeping");
+                    Thread.Sleep(5000);
                     // TODO: maybe add re-trying logic.
-                    var (bytesRead, packet) = ReadPacket(singlePacketBuffer, stream);
-                    // _logger.LogInformation(">>>>> read also done");
+
+                    (var bytesRead, var packet) = ReadPacketV2(singlePacketBuffer, client);
+                    if (bytesRead > 0)
+                    {
+                        _logger.LogInformation("missing packet: {i} was fetched", i);
+                    }
+                    // _logger.LogInformation(">>>>> read also done, bytesRead: {bytesRead}, will retry now", bytesRead);
                     if (bytesRead == 0)
                     {
+                        continue;
                         // TODO: add re-trying logic.
                     }
+
                     finalResult.Add(packet);
                     i++;
                 }
+                catch (Exception)
+                {
+                    // _logger.LogInformation("... should be retried for i: {i}", i);
+                }
             }
-            catch (Exception)
+
+            _logger.LogInformation("is finalResult null: {isNull}", finalResult is null);
+
+            foreach (var item in finalResult)
             {
-                // _logger.LogInformation("... should be retried for i: {i}", i);
+                _logger.LogInformation("seq: {seq}", item.Sequence);
             }
         }
-
-        foreach (var item in finalResult)
-        {
-            _logger.LogInformation("seq: {seq}", item.Sequence);
-        }
-
     }
 
 
-
-
-    public Result<List<ABXResponsePacket>, TimeoutException> ExecuteAllStreamRequest(string address, int port)
+    public Result<List<ABXResponsePacket>, TimeoutException> ExecuteAllStreamRequest(ABXExchangeServerClient client)
     {
         Span<byte> singlePacketBuffer = stackalloc byte[ABXResponsePacket.PACKET_SIZE_BYTES];
         var intermediateResults = new List<ABXResponsePacket>();
-        using (var client = new TcpClient())
+
+        var request = new ABXRequest { CallType = ABXRequest.STREAM_ALL, ResendSeq = 0 };
+        client.WriteRequest(request);
+        _logger.LogInformation("wrote the request, will read the packet now");
+        var readBytes = 0;
+        while (true)
         {
-            client.Connect(address, port);
-            using (var stream = client.GetStream())
+            try
             {
-                stream.ReadTimeout = 500;
-                using (var bufStream = new BufferedStream(stream))
+                var (bytesRead, packet) = ReadPacketV2(singlePacketBuffer, client);
+                if (bytesRead <= 0)
                 {
-
-                    var request = new ABXRequest { CallType = ABXRequest.STREAM_ALL, ResendSeq = 0 };
-                    request.WriteToSTream(bufStream);
-                    _logger.LogInformation("wrote the request, will read the packet now");
-                    var readBytes = 0;
-                    while (true)
-                    {
-                        try
-                        {
-                            var (bytesRead, packet) = ReadPacket(singlePacketBuffer, bufStream);
-                            if (bytesRead <= 0)
-                            {
-                                break;
-                            }
-                            intermediateResults.Add(packet);
-
-                        }
-                        catch (IOException e)
-                        {
-                            _logger.LogInformation("got IOException, trying again to read");
-                            return new Result<List<ABXResponsePacket>, TimeoutException>(new TimeoutException());
-                        }
-                    }
-                    return new Result<List<ABXResponsePacket>, TimeoutException>(intermediateResults);
-
+                    break;
                 }
+                intermediateResults.Add(packet);
+
+            }
+            catch (IOException e)
+            {
+                _logger.LogInformation("got IOException, trying again to read");
+                return new Result<List<ABXResponsePacket>, TimeoutException>(new TimeoutException());
             }
         }
+        return new Result<List<ABXResponsePacket>, TimeoutException>(intermediateResults);
     }
+    private (int, ABXResponsePacket) ReadPacketV2(Span<byte> singlePacketBuffer, ABXExchangeServerClient client)
+    {
+
+        Span<char> chars = stackalloc char[4];
+        var readBytes = client.Read(singlePacketBuffer);
+        if (readBytes <= 0)
+        {
+            return (0, null);
+        }
+        if (readBytes != ABXResponsePacket.PACKET_SIZE_BYTES)
+        {
+            throw new Exception("wrong size of the packet;");
+        }
+        var symbolBytes = singlePacketBuffer.Slice(0, 4);
+        for (var j = 0; j < 4; j++)
+        {
+            chars[j] = (char)symbolBytes[j];
+        }
+        var symbolString = new String(chars);
+        // var symbol = BinaryPrimitives.ReadUInt32BigEndian(symbolBytes);
+        var orderType = (char)singlePacketBuffer[4];
+        var quantityBytes = singlePacketBuffer.Slice(5, 4);
+        var quantity = BinaryPrimitives.ReadUInt32BigEndian(quantityBytes);
+        var priceBytes = singlePacketBuffer.Slice(9, 4);
+        var price = BinaryPrimitives.ReadUInt32BigEndian(priceBytes);
+        var seqBytes = singlePacketBuffer.Slice(13, 4);
+        var seq = BinaryPrimitives.ReadUInt32BigEndian(seqBytes);
+        // _logger.LogInformation($"symbolString: {symbolString},  orderType: {orderType}, quantity: {quantity}, price: {price}, seq: {seq}");
+        var packet = new ABXResponsePacket
+        {
+            Price = price,
+            Symbol = symbolString,
+            Quantity = quantity,
+            Sequence = seq,
+            OrderType = orderType,
+        };
+        return (readBytes, packet);
+    }
+
+
     private (int, ABXResponsePacket) ReadPacket(Span<byte> singlePacketBuffer, Stream bufStream)
     {
 
